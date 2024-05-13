9c1d7af745aca9a22d7256962a9dd112a757959e
Juan Cremades <relicary@gmail.com>
2024-05-13 22:55:40 +0200

    Added Login page to Auth Module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 
 
 @NgModule({
   declarations: [
-    LayoutPageComponent
+    LayoutPageComponent,
+    LoginPageComponent
   ],
   imports: [
     CommonModule,
