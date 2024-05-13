5cf9b69095afb5f9b70c8c95ee6afb223eee9032
Juan Cremades <relicary@gmail.com>
2024-05-13 23:19:01 +0200

    Added Lazy Routes to each Module from App Component

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -7,7 +7,6 @@ import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
-import { Error404PageComponent } from './pages/error404-page/error404-page.component';
 
 
 @NgModule({
@@ -16,8 +15,7 @@ import { Error404PageComponent } from './pages/error404-page/error404-page.compo
     LayoutPageComponent,
     ListPageComponent,
     NewPageComponent,
-    SearchPageComponent,
-    Error404PageComponent
+    SearchPageComponent
   ],
   imports: [
     CommonModule,
