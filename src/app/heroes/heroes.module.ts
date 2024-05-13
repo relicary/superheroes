4a3c4ad2a289ed700d86e59dab50d51e49e16f28
Juan Cremades <relicary@gmail.com>
2024-05-13 22:58:55 +0200

    Added Hero page to Heroes Module

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HeroesRoutingModule } from './heroes-routing.module';
+import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeroPageComponent
+  ],
   imports: [
     CommonModule,
     HeroesRoutingModule
