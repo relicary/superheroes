f4434e534846e7667bec1ab95dd5a8726f71ab75
Juan Cremades <relicary@gmail.com>
2024-05-13 22:52:44 +0200

    Added Layout pages to Auth Module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LayoutPageComponent
+  ],
   imports: [
     CommonModule,
     AuthRoutingModule
