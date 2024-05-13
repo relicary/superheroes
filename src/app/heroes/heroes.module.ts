4526323902dabce2360748597e3a546529a59240
Juan Cremades <relicary@gmail.com>
2024-05-13 22:59:27 +0200

    Added Layout page to Heroes Module

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { HeroesRoutingModule } from './heroes-routing.module';
 import { HeroPageComponent } from './pages/hero-page/hero-page.component';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 
 
 @NgModule({
   declarations: [
-    HeroPageComponent
+    HeroPageComponent,
+    LayoutPageComponent
   ],
   imports: [
     CommonModule,
