9943b9f3d0b74d7d0d75fa713c691ce96cd2abb5
Juan Cremades <relicary@gmail.com>
2024-05-13 22:59:56 +0200

    Added List page to Heroes Module

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { HeroesRoutingModule } from './heroes-routing.module';
 import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
 
 
 @NgModule({
   declarations: [
     HeroPageComponent,
-    LayoutPageComponent
+    LayoutPageComponent,
+    ListPageComponent
   ],
   imports: [
     CommonModule,
