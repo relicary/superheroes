e5093c618e37fd9601d66029e4e8bd4ef7ed2171
Juan Cremades <relicary@gmail.com>
2024-05-13 23:04:19 +0200

    Added 404 page to Shared Module

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -7,6 +7,7 @@ import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { Error404PageComponent } from './pages/error404-page/error404-page.component';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
     LayoutPageComponent,
     ListPageComponent,
     NewPageComponent,
-    SearchPageComponent
+    SearchPageComponent,
+    Error404PageComponent
   ],
   imports: [
     CommonModule,
