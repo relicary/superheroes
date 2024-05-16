af4dc2a45334c5135c91dc3dfdbb805b4ba7e76a
Juan Cremades <relicary@gmail.com>
2024-05-16 11:28:04 +0200

    Added getHeroById into Heroes Service. Load the info returned from this service into the Hero Page Component

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { Observable, catchError, of } from "rxjs";
 import { Hero } from "../interfaces/hero.interface";
 import { environments } from "../../../environments/environments";
 
@@ -13,6 +13,14 @@ export class HeroesService {
 
   getHeroes(): Observable<Hero[]> {
     return this.httpclient.get<Hero[]>(`${ this.baseUrl }/heroes`);
+  }
+
+  getHeroById(id: string): Observable<Hero | undefined> {
+    return this.httpclient
+      .get<Hero>(`${ this.baseUrl}/heroes/${ id }`)
+      .pipe(
+        catchError( error => of(undefined))
+      );
   }
 
 }
